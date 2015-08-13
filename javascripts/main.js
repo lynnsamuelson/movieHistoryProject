e38ba2406f53e5f4417e8bedbc7b19be692f9793
Ryan Tanay <ryantanay@newevolution.org>
2015-08-13 14:56:30 -0500

    Fixed formatting on main.js

--- javascripts/main.js
+++ javascripts/main.js
@@ -17,69 +17,58 @@ requirejs.config({
   }
 });
 
+requirejs(["jquery","hbs", "bootstrap",'ask-OMDB', 'firebase' ],
+function($, Handlebars, bootstrap, ask, _firebase) {
+  var tempMovies;
+  var allMovies;
+
+  $("#searchButton").click(function(evt){
+    console.log(evt);
+    ask.getMovies(function(movie) {
+      tempMovies = movie; 
+      $("#titleIn").val(movie.Title);
+      $('#actorsIn').val(movie.Actors);
+      $('#yearIn').val(movie.Year);
+    });
+    $("#titleImput").val('');
+  });
 
 
+  $("#addToMyList").click(function(addevt){
+    console.log(addevt);
+    var newMovie = {};
+    newMovie.title = $("#titleIn").val();
+    newMovie.actors = $("#actorsIn").val();
+    newMovie.year = $("#yearIn").val();
+    newMovie.rating = $("#ratingIn").val();
+    console.log("value of radio buttons", $("#seenIt").val());
+    //newMovie.seenit = $("input[name='viewed']:checked").val() === "yes" ? true : false,
+    // console.log(newMovie);
+
+    $.ajax({
+      url: "https://movies-refactored.firebaseio.com/movie.json",
+      method: "POST",
+      data: JSON.stringify(newMovie)
+    }).done(function(addedMovie) {
+      console.log("Your added movie is", addedMovie);
+    });   
+
+    $("#titleIn").val('');
+    $("#actorsIn").val('');
+    $("#yearIn").val('');
+  });
 
 
+  var myFirebaseRef = new Firebase("https://movies-refactored.firebaseio.com/");
+  myFirebaseRef.child("movie").on("value", function(snapshot) {
 
+    var movie = snapshot.val();
 
-requirejs(
-    ["jquery","hbs", "bootstrap",'ask-OMDB', 'firebase' ],
-    function($, Handlebars, bootstrap, ask, _firebase) {
-      var tempMovies;
-      var allMovies;
-
-        $("#searchButton").click(function(evt){
-          console.log(evt);
-          ask.getMovies(function(movie) {
-            tempMovies = movie; 
-            $("#titleIn").val(movie.Title);
-            $('#actorsIn').val(movie.Actors);
-            $('#yearIn').val(movie.Year);
-          });
-          $("#titleImput").val('');
-        });
-
-
-        $("#addToMyList").click(function(addevt){
-          console.log(addevt);
-
-          var newMovie = {};
-          newMovie.title = $("#titleIn").val();
-          newMovie.actors = $("#actorsIn").val();
-          newMovie.year = $("#yearIn").val();
-          newMovie.rating = $("#ratingIn").val();
-          console.log("value of radio buttons", $("#seenIt").val());
-          //newMovie.seenit = $("input[name='viewed']:checked").val() === "yes" ? true : false,
-          // console.log(newMovie);
-    
-          $.ajax({
-            url: "https://movies-refactored.firebaseio.com/movie.json",
-            method: "POST",
-            data: JSON.stringify(newMovie)
-          }).done(function(addedMovie) {
-            console.log("Your added movie is", addedMovie);
-          });   
-
-          $("#titleIn").val('');
-          $("#actorsIn").val('');
-          $("#yearIn").val('');
-
-        });
-
-
-        var myFirebaseRef = new Firebase("https://movies-refactored.firebaseio.com/");
-        myFirebaseRef.child("movie").on("value", function(snapshot) {
-
-          var movie = snapshot.val();
-
-
-          require(
-            ['hbs!../templates/movies'],
-            function(movieTemplate){
-              var populatedTemplate = movieTemplate({movie:movie});
-              $("#movie-list").html(populatedTemplate);
-            });     
-        });
-
-  });    
+    require(
+    ['hbs!../templates/movies'],
+    function(movieTemplate){
+      var populatedTemplate = movieTemplate({movie:movie});
+      $("#movie-list").html(populatedTemplate);
+    });     
+  });
+});
